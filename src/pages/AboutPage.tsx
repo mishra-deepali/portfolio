67c23cc711e8ac3da601eb811c8b19e565acfaae
Deepali Mishra <79585053+mishra-deepali@users.noreply.github.com>
2025-04-21 20:57:15 +0700

    Update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -209,11 +209,7 @@ const AboutPage = () => {
                 about potential research projects. Please feel free to reach out
                 through my contact page.
               </p>
-              <style>
-                .paragraph {
-                    margin-bottom: 20px;
-                 }
-              </style>
+              
 
             </div>
           </motion.section>
